5dccf9e219f518020acafc73a0659c81f1eadb1c
Shejal Gauns <86370411+Shejal987@users.noreply.github.com>
2023-08-02 12:22:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1_1_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new string[] { "flower", "house", "tree" };
+
+            /*var result= from element in words select element;
+            Console.WriteLine(result);
+
+            foreach (var item in words) {
+                Console.WriteLine(item);
+            }*/
+
+            /*var result= from element in words where element.Contains("r") select element;
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }*/
+
+            var result = words.Where(ele => ele.Contains("r"));
+            foreach ( var item in result )
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
